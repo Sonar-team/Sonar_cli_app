86833bd946c34766d28b6c51a4dee545756d4990
akmot9 <avicocyprien@yahoo.com>
2023-10-20 10:32:52 +0200

    add readme

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,30 @@ use clap::Parser;
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
+    /// Name the output name of the csv
     #[arg(short, long)]
-    name: String,
+    output: String,
     #[arg(short, long, default_value_t = 1)]
-    count: u8,
+    /// give the interface name to scan
+    network: u8,
+    /// Give the scan time 
+    #[arg(short, long, default_value_t = 0)]
+    time: u32,
 }
 
 fn main() {
+    // ASCII art banner
+    let banner = r#"
+     _________                           
+    /   _____/ ____   ____ _____ _______ 
+    \_____  \ /  _ \ /    \\__  \\_  __ \
+    /        (  <_> )   |  \/ __ \|  | \/
+   /_______  /\____/|___|  (____  /__|   
+           \/            \/     \/          
+    "#;
+
+    println!("{}", banner);
+
     let args = Args::parse();
-    for _ in 0..args.count {
-        println!("Hello {}!", args.name);
-    }
-} 
+    println!("{} {}",args.network, args.output)
+}
