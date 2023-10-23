671d65b20b9b48dd9eaa3ddbc3ea50975d369eac
akmot9 <avicocyprien@yahoo.com>
2023-10-23 10:18:37 +0200

    unit test main

--- src/main.rs
+++ src/main.rs
@@ -30,3 +30,21 @@ fn main() {
     let args = Args::parse();
     println!("{} {}",args.network, args.output)
 }
+
+// Import necessary modules for testing
+#[cfg(test)]
+mod tests {
+    use super::*; // Import items from the main module
+
+    // Test case for parsing command-line arguments
+    #[test]
+    fn test_parse_args() {
+        // Simulate command-line arguments for testing
+        let args = Args::parse_from(&["myapp", "-n", "42", "--output", "output.csv"]);
+
+        // Check if the arguments were parsed correctly
+        assert_eq!(args.network, 42);
+        assert_eq!(args.output, "output.csv");
+        assert_eq!(args.time, 0); // Default value
+    }
+}
